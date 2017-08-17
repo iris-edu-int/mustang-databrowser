6c051e4856826505cfbc96b35a93ec46a649917e
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2017-08-17 15:56:25 -0700

    fleshing out buildMiniCRAN.R

--- buildMiniCRAN.R
+++ buildMiniCRAN.R
@@ -1,11 +1,8 @@
-#!/usr/bin/env Rscript
-
 # This script uses the miniCRAN package to create a local CRAN repository
 # will all packages source required to build the MUSTANG Databrowser.
 
 VERSION = "0.0.1"
 
-library(methods)       # always included for Rscripts
 library(miniCRAN)      # to create a local CRAN repository
 
 # Create a new miniCRAN directory
@@ -19,6 +16,16 @@ pkgs <- c("IRISMustangMetrics",
 repos <- c(CRAN="http://cran.microsoft.com")
 Rversion <- R.version  # opportunity to override system default
 
+# localRepo is a path in the source tree that will contain all of the package source code
+localRepo <- normalizePath("./miniCRAN")
+
+# deployedLib is a path available to the deployed mustang databrowser that will contain 
+# coompiled versions of all the packages.
+deployedLib <- normalizePath("./R_Libraries")
+
+
+# ----- Create a miniCRAN repository ------------------------------------------
+
 # Find all dependencies
 allPackages <- pkgDep(pkg=pkgs,
                       repos=repos,
@@ -27,9 +34,17 @@ allPackages <- pkgDep(pkg=pkgs,
 
 # Createa the miniCRAN repo
 pkgSource <- makeRepo(allPackages,
-                      path="./miniCRAN",
+                      path=localRepo,
                       type="source",
                       Rversion=Rversion)
 
-# Add MazamaWebUtils
-addLocalPackage(pkg="MazamaWebUtils", pkgPath="~/Projects/MazamaScience", path="./miniCRAN", build=TRUE)
+# Add MazamaWebUtils from Jon's desktop machine
+addLocalPackage(pkg="MazamaWebUtils", pkgPath="~/Projects/MazamaScience", path=localRepo, build=TRUE)
+
+
+# ----- Install to the deployed directory -------------------------------------
+
+# Install with:
+for ( package in allPackages ) {
+  install.packages(package, lib=deployedLib, repos=paste0("file:///",localRepo), type="source")
+}
